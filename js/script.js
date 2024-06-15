07c01d2ed63c5a90b4589b794724d82d9bc29e25
Alexandra Demetriades <amdemetriades@gmail.com>
2024-06-15 20:21:11 +0100

    added a limit to the number of images to load initially and a load more button

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-const container = document.querySelector(".cat-container");
+// const toggleBtn= document.getElementsByClassName('toggle-button');
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.cat-container');
+    const loadMoreButton = document.querySelector('#load-more');
+    let catData = [];
+    let currentIndex = 0;
+    const limit = 6;
 
-fetch('https://cataas.com/api/cats')
-    .then((res) => {
-        if(!res.ok) {
-            throw new Error('Network response error. Needs a hug. And a cat.')
-        } 
-        return res.json();
-    }) 
-    .then((catData) => {
-        console.log(catData);
-        getCats(catData);
-    })
-    .catch((error) => console.log(error));
+    fetch('https://cataas.com/api/cats')
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Network response error. Needs a hug. And a cat.');
+            }
+            return res.json();
+        })
+        .then((data) => {
+            catData = data;
+            displayCats();
+        })
+        .catch((error) => console.log(error));
 
-    function getCats (catData) {
-        catData.forEach(cat => {
-            const singleCat = document.createElement('li'); //creating a li item to append to the ul
+    function displayCats() {
+        const endIndex = currentIndex + limit; // 0 + 6 to limit images to 6
+        const catsToDisplay = catData.slice(currentIndex, endIndex); // slices the array to get the next set of images
+
+        catsToDisplay.forEach(cat => {
+            const singleCat = document.createElement('li'); // creating a li item to append to the ul
             const catImg = document.createElement('img');
-            catImg.setAttribute('max-height', '300px');
-            catImg.setAttribute('width', 'auto');
-            catImg.setAttribute('object-fit', 'contain')
             
             catImg.src = `https://cataas.com/cat/${cat._id}`;
             catImg.alt = cat.tags.join(', '); // adding alt text for the images generated
 
             singleCat.appendChild(catImg);
             container.appendChild(singleCat);
-        }) 
+        });
+        currentIndex = endIndex; // updates the current index with where we are in the array
+
+        if (currentIndex >= catData.length) {
+            loadMoreButton.style.display = 'none';
+        }
     }
+
+    loadMoreButton.addEventListener('click', displayCats);
+});
+
+
+// function toggleMode() {
+//     toggleBtn.addEventListener('click', () => {
+//         const background = document.getElementsByName('body');
+
+//         background.classList.toggle('.cat-mode');
+//     })
+// }
